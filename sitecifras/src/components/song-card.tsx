32538c7349ea9bdd0bf389abbeee1c7a37d2dd56
Marcos Jhonata <marcosjhonata23@gmail.com>
2026-05-26 10:56:11 -0300

    mudança na posição do botão deletar nas musicas

--- sitecifras/src/components/song-card.tsx
+++ sitecifras/src/components/song-card.tsx
@@ -8,16 +8,10 @@ type SongCardProps = {
 
 function SongCard({ musica, removerMusica }: SongCardProps) {
 	return (
-		<li className="flex flex-col gap-2">
+		<li className="flex flex-line gap-2">
 			<div className="flex flex-wrap items-center justify-between gap-3">
 				<strong>{musica.musica}</strong>
-				<button
-					type="button"
-					onClick={() => removerMusica(musica.id)}
-					className="rounded-lg bg-red-600 px-3 py-1 font-semibold text-white"
-				>
-					Remover
-				</button>
+			
 			</div>
 			<div className="flex flex-wrap items-center gap-3">
 				<span className="font-semibold">Acordes:</span>
@@ -26,9 +20,18 @@ function SongCard({ musica, removerMusica }: SongCardProps) {
 						key={`${musica.id}-${acorde}-${index}`}
 						className="min-w-11 rounded-full bg-blue-100 px-3 py-1 text-center font-bold text-blue-600"
 					>
+						
 						{acorde}
 					</span>
+					
 				))}
+					<button
+					type="button"
+					onClick={() => removerMusica(musica.id)}
+					className="rounded-lg bg-red-600 px-3 py-1 font-semibold text-white"
+				>
+					Remover
+				</button>
 			</div>
 		</li>
 	);
